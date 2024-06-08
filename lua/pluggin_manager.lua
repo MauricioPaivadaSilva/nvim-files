b08ff90a2cf57080a2e2715bcb0b687610d4d626
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-06-08 19:43:36 -0300

    Retirados os plugins do arquivo pluggin_manager tornando-o apenas um gerenciador com a capacidade de importar os plugins.

--- lua/pluggin_manager.lua
+++ lua/pluggin_manager.lua
@@ -1,38 +1,6 @@
 local lazy = require("lazy")
 
 local plugins = {
-	--
-	--	Nvim Tree
-	--
-	{
-		'nvim-tree/nvim-tree.lua',
-		dependencies = {
-			'nvim-tree/nvim-web-devicons'
-		},
-		config = function()
-			require('nvim-tree').setup()
-		end
-	},
-
-	--
-	--	Theme Catppuccin
-	--
-	{
-		'catppuccin/nvim',
-		lazy = true,
-		name = 'catppuccin',
-		config = function()
-			require('catppuccin').setup({
-				flavour = "mocha"
-			})
-		end
-	},
-
-	--
-	--	rust.vim
-	--
-	{
-		"rust-lang/rust.vim"
-	}
+	{	import = "plugins"	}
 }
 lazy.setup(plugins)
